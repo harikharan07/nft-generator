89493b0a357eb2b09b23081cbd44d6a94cd1293e
walz0 <aidan.m.walz@gmail.com>
2021-08-31 17:21:42 -0400

    Added loading for trait data and supply generation

--- nft.py
+++ nft.py
@@ -1,4 +1,7 @@
 from PIL import Image
+import json
+from enum import Enum
+import numpy as np
 import random
 
 """
@@ -6,49 +9,40 @@ import random
     function to generate metadata for all 10000 images
 """
 
-hats = {
-    "trapper": {
-        "url": "./hats/trapper.png",
-        "supply": 0 
-    },
-    "scuba": {
-        "url": "./hats/scuba.png",
-        "supply": 0 
-    },
-    "cowboy": {
-        "url": "./hats/cowboy.png",
-        "supply": 0 
-    },
-    "astronaut": {
-        "url": "./hats/astronaut.png",
-        "supply": 0 
-    }
-}
-# i = random.randint(0, len(list(hats.keys())) - 1)
+TOTAL_ITEMS = 10000
 
-def getRarity(min, max, totalItems, curveType):
-    if curveType == "exp":
-        # Solve for exponential function
-        a = min
-        b = max/min
-        func = lambda x : min * (b**x)
-        return [func(x / totalItems) for x in range(1, totalItems + 1)]
-    elif curveType == "linear":
-        m = max - min
-        b = min
-        return [(m * (x / totalItems)) + b for x in range(1, totalItems + 1)]
-    else:
-        return None
+class Trait(Enum):
+    BACKGROUND = 0
+    HAT = 1
+    EYES = 2
+    FUR = 3
+    FACE = 4
+    CLOTHES = 5
+    
+#  i = random.randint(0, len(list(hats.keys())) - 1)
 
-rarity = getRarity(0.5, 19, 12, "exp")
+def loadData():
+    obj = {}
+    for trait in Trait:
+        with open("./data/{0}.json".format(trait.name.lower())) as data:
+            obj[trait.name.lower()] = json.loads(data.read())
+    return obj
 
-# for _ in rarity:
-#     print(_)
+def getSupply(weights):
+    total = sum(weights)
+    supply = []
+    for w in weights:
+        supply += [int((w / total) * TOTAL_ITEMS)]
+    # If supplies do not total properly, add 1 randomly
+    if sum(supply) != TOTAL_ITEMS:
+        i = random.randint(0, len(supply) - 1)
+        supply[i] += 1
+    return supply
 
-for i in range(len(list(hats.keys()))):
-    hat = list(hats.keys())[i]
-    panda = Image.open("panda2.png")
-    hat = Image.open(hats[hat]["url"])
+# for i in range(len(list(hats.keys()))):
+#    hat = list(hats.keys())[i]
+#    panda = Image.open("panda2.png")
+#    hat = Image.open(hats[hat]["url"])
 
-    panda.paste(hat, (0, 0), hat)
-    panda.save("test{0}.jpg".format(i))
+#    panda.paste(hat, (0, 0), hat)
+#    panda.save("./output/test{0}.jpg".format(i))
