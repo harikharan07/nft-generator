d4c83a4a390286e63bc137ed6df5dcae03122116
walz0 <aidan.m.walz@gmail.com>
2021-09-07 01:03:07 -0400

    Fixed wings, added background to gitignore

--- nft.py
+++ nft.py
@@ -10,7 +10,7 @@ import random
     function to generate metadata for all 10000 images
 """
 
-TOTAL_ITEMS = 10000
+TOTAL_ITEMS = 9900
 
 class Trait(Enum):
     BACKGROUND = 0
@@ -19,28 +19,11 @@ class Trait(Enum):
     CLOTHES = 3
     ACCESSORIES = 4
     FACE = 5
-    HAT = 6
+    HEAD = 6
 
-TRAIT_SUPPLY = {
-    "background": {
-        "total": 9995
-    },
-    "clothes": {
-        "total": 9995
-    },
-    "eyes": {
-        "total": 9995
-    },
-    "face": {
-        "total": 9995
-    },
-    "fur": {
-        "total": 9995
-    },
-    "hat": {
-        "total": 9995
-    }
-}
+# Load trait data
+f = open("./data/supply.json", "r")
+TRAIT_SUPPLY = json.loads(f.read())
 
 def loadData(trait):
     obj = {}
@@ -58,22 +41,19 @@ def generateJSON():
                 items = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
                 delimiters = ["background", "face", "fur", "eyes"]
                 if not trait.name.lower() in delimiters:
-                    print(trait.name.lower())
                     output["none"] = {
                         "url": "./misc/none.png",
                         "trait_type": trait.name.lower(),
-                        "supply": 0,
                         "layer": trait.value
                     }
                 for item in items:
                     # Layer exception for wings
                     layer = trait.value
-                    if "wings" in item:
+                    if "wings" in item.lower():
                         layer = trait.value - 1
                     output[item[:item.index(".png")]] = {
                         "url": "./" + trait.name.lower() + "/" + item,
                         "trait_type": trait.name.lower(),
-                        "supply": 0,
                         "layer": layer 
                     }
                 file.write(json.dumps(output))
@@ -93,29 +73,52 @@ def getMetadata(index, attributes):
         }]
     return output
 
-def generateImages():
+def generateImages(amount):
+    global TRAIT_SUPPLY
     generateJSON()
-    for _ in range(100):
+    for _ in range(amount):
         layers = []
         panda = Image.new(mode="RGBA", size=(2700, 2700), color=(255, 255, 255))
+        # Attributes list for metadata
+        attributes = []
         # Generate list of attributes
         for trait in Trait:
             # Get trait JSON
             data = loadData(trait)
-            # Random number 
-            i = random.randint(0, len(list(data.keys())) - 1)
-            # Random attribute
-            attr = list(data.keys())[i]
+            # Create container for attribute
+            attr = None
+            while(attr == None):
+                # Random number 
+                i = random.randint(0, len(list(data.keys())) - 1)
+                # Random attribute
+                temp = list(data.keys())[i]
+                # If there is supply assign attr
+                if TRAIT_SUPPLY[trait.name.lower()][temp] > 0:
+                    attr = temp
+            # Decrement supply
+            TRAIT_SUPPLY[trait.name.lower()]["total"] -= 1
+            TRAIT_SUPPLY[trait.name.lower()][attr] -= 1
+            # Add attributes to metadata list
+            attributes += [{
+                "trait_type": trait.name.lower(),
+                "value": attr
+            }]
             layers.append(data[attr])
+        # Pull metadata from layers
+        metadata = getMetadata(_, attributes)
+        f = open("./output/metadata/data.json", "w+")
+        f.write(json.dumps(metadata))
+        f.close()
         # Sort list ascending
         layers.sort(key=lambda x:x["layer"])
         # Iterate and swap if wings
         for i in range(len(layers)):
-            if "wings" in layers[i]["url"]:
+            # If wings, swap layers
+            if "wings" in layers[i]["url"].lower():
                 layers[i-1]["layer"] += 1
+                print(layers[i-1])
         # Sort again
         layers.sort(key=lambda x:x["layer"])
-        print(_, layers)
         # Create image
         for layer in layers:
             attr_img = Image.open(layer["url"]).convert("RGBA")
@@ -130,6 +133,4 @@ def getSupply(trait, supply):
     print(data)
     pass
 
-generateImages()
-# getSupply(Trait.EYES)
-# generateJSON()
+generateImages(9995)
